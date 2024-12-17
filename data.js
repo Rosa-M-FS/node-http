42e421ec1055e0a1dcde8341cff5e78c0b6a2d9a
Rosa-M-FS <rmlopezrobles@gmail.com>
2024-12-17 19:08:50 +0100

    Ejercicio Terminado

---
+++ data.js
@@ -0,0 +1,13 @@
+const data={
+    title: 'Primera página Node.js',
+    subtitle: 'Programación en http',
+    description:'Página para aprender y prácticar primeros conceptos en node.js y http',
+    author:'Rosa M.',
+    createDate:'17-12-2024',
+    contact:{
+        email:'example@nodejs.com',
+        phone:'+34657776110'
+    }
+};
+
+module.exports= {data};
---
+++ server.js
@@ -0,0 +1,34 @@
+const data=require('./data.js');
+
+const http = require('node:http');
+const server=http.createServer((req,res)=>{
+    res.end(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>${data.title}</title>
+        </head>
+        <body>
+            <header>
+                <h1>${data.title}</h1>
+                <h2>${data.subtitle}</h2>
+            </header>
+            <div>
+                <p>${data.description}</p>
+                <p><em>${data.author}</em></p>
+            </div>
+            <footer>
+                <p><strong>Contacto</strong></p>
+                <ul>
+                    <li>Email: ${data.email}</li>
+                    <li>Teléfono: ${data.phone}</li>
+                </ul>
+            </footer>
+        </body>
+        </html>`)
+});
+server.listen(3000,()=>{
+    console.log(`Server listening on port http://localhost${server.address().port}`)
+})
